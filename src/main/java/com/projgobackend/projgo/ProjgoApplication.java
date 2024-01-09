e5c3909e9232f555aadb5891dde0569bad6eb836
mjustatool <fousenaibdisbdis@gmail.com>
2024-01-09 16:45:04 +0100

    config the project and add swagger modelmapper dependency

---
+++ src/main/java/com/projgobackend/projgo/ProjgoApplication.java
@@ -0,0 +1,13 @@
+package com.projgobackend.projgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projgobackend/projgo/ProjgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projgobackend.projgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
